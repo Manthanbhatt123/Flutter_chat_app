e193f794a52a878d174d20f8e1cb21daa5c9d882
Manthan <manthan.bhatt7575@gmail.com>
2024-11-24 18:53:25 +0530

    -Update profile user screen

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -9,6 +9,10 @@ class APIs {
   //For User authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
 
+  static User get myUser => auth.currentUser!;
+
+  static late ChatUser me;
+
   //for accessing cloud firestore database
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -21,24 +25,57 @@ class APIs {
         .exists;
   }
 
+  static Future<void> selfInfo() async {
+    await firestore
+        .collection('users')
+        .doc(myUser.uid)
+        .get()
+        .then((user) async {
+      if (user.exists) {
+        me = ChatUser.fromJson(user.data()!);
+        log("MyData from FireStore \n ${user.data()}");
+      } else {
+        createUser("user");
+      }
+    });
+  }
+
   //for creating user
-  static Future<void> createUser() async {
+  static Future<void> createUser(String name) async {
     final time = DateTime.now().millisecondsSinceEpoch.toString();
 
     final chatUser = ChatUser(
-        image: auth.currentUser!.photoURL ?? '',
-        name: auth.currentUser!.displayName?? "",
+        image: myUser.photoURL ?? '',
+        name: myUser.displayName ?? name,
         about: "Hello Developer",
         createdAt: time,
         lastActive: time,
         isOnline: false,
-        id: auth.currentUser!.uid,
+        id: myUser.uid,
         pushToken: '',
-        email: auth.currentUser!.email.toString());
-    
-    // log(auth.currentUser!.toString());
+        email: myUser.email.toString());
+
+    // log(myUser.toString());
     return await firestore
-            .collection('users')
-            .doc(auth.currentUser!.uid).set(chatUser.toJson());
+        .collection('users')
+        .doc(myUser.uid)
+        .set(chatUser.toJson());
+  }
+
+  static Stream<QuerySnapshot<Map<String, dynamic>>> getAllUsers() {
+    return firestore
+        .collection('users')
+        .where('id', isNotEqualTo: myUser.uid)
+        .snapshots();
+  }
+
+  //for checking user existence
+  static Future<void> updateUserInfo() async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update(
+      {
+        'name': me.name,
+        'about':me.about
+      },
+    );
   }
 }
